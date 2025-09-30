06f777d7a07356141a16d6d6a0be35819d9a8eb0
VDN <victor.d.nikolaev@gmail.com>
2025-09-30 15:45:43 +0300

    init commit

---
+++ 01/01.ino
@@ -0,0 +1,18 @@
+void setup() {
+	pinMode(4, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(6, OUTPUT);
+	pinMode(7, OUTPUT);
+}
+
+void setMotors(int L, int R) {
+	L = constrain(L, -255, 255);
+	R = constrain(R, -255, 255);
+
+	digitalWrite(4, L < 0);
+	analogWrite(5, abs(L));
+	analogWrite(6, abs(R));
+	digitalWrite(7, R < 0);
+}
+
+void loop() {}
