63dc5ca322c467cac64503848e516dce17bd90b1
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-10-06 20:47:59 +0300

    Добавлены поясняющие комментарии в скетч

--- 01/01.ino
+++ 01/01.ino
@@ -1,4 +1,5 @@
 void setup() {
+    // Установка пинов драйвера моторов как ВЫХОД
 	pinMode(4, OUTPUT);
 	pinMode(5, OUTPUT);
 	pinMode(6, OUTPUT);
@@ -6,13 +7,21 @@ void setup() {
 }
 
 void setMotors(int L, int R) {
-	L = constrain(L, -255, 255);
-	R = constrain(R, -255, 255);
-
+	// 1. Сначала устанавливаем направления (Определяем по знаку скорости)
 	digitalWrite(4, L < 0);
-	analogWrite(5, abs(L));
-	analogWrite(6, abs(R));
 	digitalWrite(7, R < 0);
+
+	// 2. Берём модуль (абсолютное значение) скоростей (ШИМ)
+	L = abs(L);
+	R = abs(R);
+
+	// 3. Ограничиваем значения ШИМ
+	L = constrain(L, 0, 255);
+	R = constrain(R, 0, 255);
+
+    // 4. Подаём ШИМ на пины драйвера для установки скорости моторов
+	analogWrite(5, L);
+	analogWrite(6, R);
 }
 
 void loop() {}
