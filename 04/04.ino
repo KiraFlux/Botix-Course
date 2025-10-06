4cccf69c15fe23c6d6930fd60b2fe6c6a7d36c06
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-10-06 21:28:08 +0300

    - Более детально расписан алгоритм торможения - Одноразовый тест перенесён в setup. - Добавлен параметр в процедуру торможения (длительность)

--- 04/04.ino
+++ 04/04.ino
@@ -1,31 +1,49 @@
+void setMotors(int L, int R) {
+	digitalWrite(4, L < 0);
+	digitalWrite(7, R < 0);
+	analogWrite(5, constrain(abs(L), 0, 255));
+	analogWrite(6, constrain(abs(R), 0, 255));
+}
+
+// Функция торможения (duration - длительность торможения в мс)
+void fastStop(int duration = 400) {
+	// 1. Рассчитываем время окончания торможения
+	auto end = millis() + duration;
+
+	// 2. Определим переменную для чередования скорость-направления
+	int s = 255;
+
+	// 3. Пока не наступил момент окончания торможения:
+	while (millis() < end) {
+
+	    // 4. Устанавливаем моторы в одном направлении
+		setMotors(s, s);
+
+		// 5. Ожидаем малый период (обычно от 1 до 10 мс)
+		delay(5);
+
+		// 6. Меняем направление моторов сменой знака скорости
+		s = -s;
+	}
+
+    // 7. Выключаем моторы - торможение завершено.
+	setMotors(0, 0);
+}
+
 void setup() {
 	pinMode(4, OUTPUT);
 	pinMode(5, OUTPUT);
 	pinMode(6, OUTPUT);
 	pinMode(7, OUTPUT);
-}
 
-void setMotors(int L, int R) {
-	L = constrain(L, -255, 255);
-	R = constrain(R, -255, 255);
+    // Тест торможения
 
-	digitalWrite(4, L < 0);
-	analogWrite(5, abs(L));
-	analogWrite(6, abs(R));
-	digitalWrite(7, R < 0);
-}
-
-void fastStop() {
-	for (int i = 0, s = 255; i < 30; ++i, s = -s) {
-		setMotors(s, s);
-		delay(5);
-	} setMotors(0, 0);
-}
-
-void loop() {
+    // 1. Полный вперёд! (на 1 секунду)
 	setMotors(255, 255);
 	delay(1000);
 
+    // 2. Исполняем торможение
 	fastStop();
-	exit(0);
 }
+
+void loop() {}
